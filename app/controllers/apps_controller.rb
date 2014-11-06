48c93b2de3a00dad4d4c77ae8d3327fd3e10ac8f
richarkc <richarkc@miamioh.edu>
2014-11-06 02:13:30 -0500

    Initial commit, basic website with form

---
+++ app/controllers/apps_controller.rb
@@ -0,0 +1,50 @@
+class AppsController < ApplicationController
+  def index
+    @apps= App.all
+    
+  end
+  def new #get
+    @apps = App.new
+  end
+  def create #post
+    @apps = App.new
+    @apps.name = params[:apps][:name]
+    @apps.developer = params[:apps][:developer]
+    @apps.price = params[:apps][:price]
+    @apps.link = params[:apps][:link]
+    
+    if @apps.save
+      flash[:notice] = "Succesfully saved new app"
+      redirect_to "/apps"
+    else
+      flash[:notice] = "Failed to save app"
+      redirect_to "/apps/new"
+    end
+    
+  end
+  def edit #get
+    @apps = App.find(params[:id]) rescue nil
+    
+  end
+  def update #post
+    @apps = App.find(params[:id]) rescue nil
+  
+    unless @apps.nil? 
+    @apps.name = params[:apps][:name]
+    @apps.developer = params[:apps][:developer]
+    @apps.price = params[:apps][:price]
+    @apps.link = params[:apps][:link]
+    
+      if @apps.save
+        flash[:notice] = "Succesfully saved app"
+        redirect_to "/apps/"
+      else
+        flash[:notice] = "Failed to save app"
+        redirect_to "/#{@apps.id}/edit"
+      end
+      return
+    end
+    flash[:notice] = "Couldn't find an app with that id"
+    redirect_to "/"
+  end
+end
---
+++ db/migrate/001_add_table_apps.rb
@@ -0,0 +1,17 @@
+class AddTableApps < ActiveRecord::Migration
+  def self.up
+    create_table :apps do |t|
+      t.column :name, :string
+      t.column :developer, :string
+      t.column :price, :decimal
+      t.column :link, :string
+      
+      t.timestamps
+      
+    end
+  end
+  def self.down
+    drop_table :apps
+    
+  end
+end
