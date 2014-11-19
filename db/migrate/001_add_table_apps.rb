b95848bf68dd337c36abaff03d30a59b8f31c198
Kyle Busdieker <kcbusdieker@gmail.com>
2014-11-18 20:35:39 -0500

    Added the users and comments tables to the correct place (I think).

--- db/migrate/001_add_table_apps.rb
+++ db/migrate/001_add_table_apps.rb
@@ -14,4 +14,35 @@ class AddTableApps < ActiveRecord::Migration
     drop_table :apps
     
   end
+  
+  def self.up
+    create_table :users do |t|
+      t.column :username, :string
+      t.column :password, :string
+      t.column :permissions, :integer #0 for administrator, 1 for moderator, 2 for normal user
+      
+      t.timestamps
+      
+    end
+  end
+  def self.down
+    drop_table :users
+    
+  end
+  
+  def self.up
+    create_table :comments do |t|
+      t.column :commentor, :string
+      t.column :comment, :string
+      t.column :appID, :integer
+      
+      t.timestamps
+      
+    end
+  end
+  def self.down
+    drop_table :comments
+    
+  end
+  
 end
